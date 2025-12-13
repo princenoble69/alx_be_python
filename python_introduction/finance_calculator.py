d1f88efc9aa3f527f92222a8d40ca618226fb2d4
Noble Amfo <nobleboakye97@gmail.com>
2025-12-13 03:27:33 +0000

    fix: Updated finance_calculator.py calculaton logic

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,6 @@
 monthly_income = int(input("Enter your monthly income: "))
 monthly_expense = int(input("Enter your total monthly expenses: "))
 monthly_savings = (monthly_income - monthly_expense)
-simple_annual_interest_rate = 0.05
-Projected_savings = monthly_savings * 12 + (monthly_savings * 12 * simple_annual_interest_rate)
+Projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 print(f"Your monthly savings are: {monthly_savings}")
 print(f"Projected savings after one year, with interest, is: {Projected_savings}")

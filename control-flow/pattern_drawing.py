c8b611a254bd2e4ffc9456d1a1cd3e6411a10abd
Noble Amfo <nobleboakye97@gmail.com>
2025-12-14 18:21:38 +0000

    feat: Add pattern_drawing.py in control-flow

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,26 @@
+size = 0
+pattern = 0
+
+try:
+    size = input("Enter the size of the pattern:")
+    pattern = int(size)
+
+except ValueError:
+    print("ERROR: ENTER NUMERICAL VALUES ONLY")
+    exit()
+
+if pattern <= 0:
+    print("Error: number must be positive")
+    exit()
+
+ash = '*'
+row = 0
+
+while row < pattern:
+    
+    for col in range(pattern):
+        print(ash, end="") 
+    
+    print()
+    
+    row += 1

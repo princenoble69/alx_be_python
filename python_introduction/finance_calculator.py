c2f9b3182df1b12a15750e8b4335faee4eca0941
princenoble69 <nobleboakye97@gmail.com>
2026-01-15 10:35:31 +0000

    Change income and expense inputs to float type

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,9 @@
-monthly_income = int(input("Enter your monthly income: "))
-monthly_expense = int(input("Enter your total monthly expenses: "))
-monthly_savings = (monthly_income - monthly_expense)
-Projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-print(f"Your monthly savings are: {monthly_savings}")
-print(f"Projected savings after one year, with interest, is: {Projected_savings}")
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+
+monthly_savings = monthly_income - monthly_expenses
+
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+print(f"Your monthly savings are ${int(monthly_savings)}.")
+print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
